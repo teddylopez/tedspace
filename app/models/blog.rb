539e6c46f716224ade62bf94ef65f2641966ecbe
teddylopez <tedmlopez@gmail.com>
2018-04-14 19:32:51 -0400

    Integrated relationship between blogs and topics.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,7 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, :use => :slugged
   validates_presence_of :title, :body
+  belongs_to :topic
 
   enum status: { draft: 0, published: 1 }
 end
