75ae06494b56565a5253c220d7eb8f57b8ce7bff
teddylopez <tedmlopez@gmail.com>
2018-04-14 20:06:24 -0400

    Implemented default images for portfolio items.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,15 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+  after_initialize :set_defaults
 
   def self.react
     where(subtitle: "React js")
   end
 
   scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
