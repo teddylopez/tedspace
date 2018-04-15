fc3832167688f546eeecb1051e1f86c632e52d39
teddylopez <tedmlopez@gmail.com>
2018-04-14 20:57:39 -0400

    Integrated nested attributes for portfolio model.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,6 +3,8 @@ class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
   after_initialize :set_defaults
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attr['name'].blank? }
 
   def self.react
     where(subtitle: "React js")
