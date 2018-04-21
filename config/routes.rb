c44dc4fe26cc127b9fe696aa7627094204918d3a
teddylopez <tedmlopez@gmail.com>
2018-04-21 15:29:59 -0400

    Implemented initial connection to Twitter API.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'about-ted', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'twitter', to: 'pages#twitter'
 
   resources :blogs do
     member do
