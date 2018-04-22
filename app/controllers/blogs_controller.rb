de0bf73fec8c46314455f120b9f0f8fe332c14f4
teddylopez <tedmlopez@gmail.com>
2018-04-21 21:01:24 -0400

    Updated blog index to descending order.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    @blogs = Blog.all.order("created_at DESC").page(params[:page]).per(5)
     @page_title = "tedspace | my blogs"
   end
 
