9065a2303bc3b5cb6c951e6d4f7955988fa77360
teddylopez <tedmlopez@gmail.com>
2018-04-23 20:12:20 -0400

    Modifying configs for prod.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.assets.paths << "#{Rails.root}/app/assets/videos"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -28,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
