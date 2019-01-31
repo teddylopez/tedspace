4d3742b4b9c1bddd723880862b17a480d47132f3
teddylopez <tedmlopez@gmail.com>
2019-01-31 16:33:45 -0500

    Integrate gritter for alerts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require rails-ujs
 //= require jquery-ui
 //= require cocoon
+//= require gritter
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -53,6 +53,18 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, :title => "tedlopez", sticky: false, time: 1000)
+  end
+
   def copyright_generator
     LopezViewTool::Renderer.copyright 'Ted López', 'All rights reserved'
   end
