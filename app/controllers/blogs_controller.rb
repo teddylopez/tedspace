baf031d639d3a82fd9f52a17be3d77689774fda3
teddylopez <tedmlopez@gmail.com>
2019-02-04 20:04:53 -0500

    List blog topics in sidebar

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
   # GET /blogs
@@ -79,6 +80,11 @@ class BlogsController < ApplicationController
   end
 
   private
+
+    def set_sidebar_topics
+      @side_bar_topics = Topic.with_blogs
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.friendly.find(params[:id])
