bf8c30dec28447fdada48f0c709cc2342a4196aa
Ted Lopez <ted.lopez@nationals.com>
2019-02-16 23:47:33 -0500

    Add text-shadow

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,7 +1,6 @@
 $( document ).on('turbolinks:load', function() {
-  
+
   $('.menu-toggle').on('click', function() {
-    console.log('clicked')
     $('.site-nav--close').toggleClass('site-nav--open');
   });
 
