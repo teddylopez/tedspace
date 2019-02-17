0b30ddb8c6a6cd018cf3fabf254231e66c180db4
Ted Lopez <ted.lopez@nationals.com>
2019-02-17 12:25:37 -0500

    Improve nav with opacity

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,7 +1,12 @@
 $( document ).on('turbolinks:load', function() {
 
-  $('.menu-toggle').on('click', function() {
-    $('.site-nav--close').toggleClass('site-nav--open');
+  $('.menu-toggle').on('click', function(e) {
+    $('#nav-list').attr("class","show");
+  });
+
+  $('#nav-list').on('click', function(e) {
+    console.log(e)
+      $('#nav-list').attr("class","hidden");
   });
 
 });
