87de1f7e64af3dda189532af04ea305f9df9bc2d
Ted Lopez <ted.lopez@nationals.com>
2019-02-17 23:23:00 -0500

    Add dynopoker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,11 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+
+  Dynopoker.configure do |config|
+    config.address = 'http://wakemydyno.com'
+    #  config.enable = false # default is true
+    #  config.poke_frequency = 123 # default is 1800s (30min)
+  end
+  
 end
