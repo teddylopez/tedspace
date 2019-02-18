a064cb1617579504d3f30b2db3eb284ab934c6bb
Ted Lopez <ted.lopez@nationals.com>
2019-02-18 10:50:42 -0500

    Adjust production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://tedspace-redesign.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'http://tedlopez.us'}
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
@@ -107,5 +107,5 @@ Rails.application.configure do
     #  config.enable = false # default is true
     #  config.poke_frequency = 123 # default is 1800s (30min)
   end
-  
+
 end
