5ca649a1c8a37b6ef42d39eebc027e03c3d26eda
Ted Lopez <ted.lopez@nationals.com>
2019-02-22 12:34:12 -0500

    Adjust asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( blogs.scss )
-Rails.application.config.assets.precompile += %w( portfolios.scss )
+Rails.application.config.assets.precompile += %w(  about.scss application.scss blogs.scss contacts.scss custom-devise-forms.scss nav.scss noise.scss portfolios.scss topics.scss )
